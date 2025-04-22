8484b3179d5d4e473828a17c762c5b932b200d63
ruchu8 <o0o08l@outlook.com>
2025-04-22 21:28:53 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,16 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'MJJ网站监控',
+  SiteName: '如初的网站监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2828404-32ec189c0d47d45b7f0a207a',
+    'ur2885243-68d3e697797d180c9b2cdd06',
   ],
 
   // 日志天数
-  CountDays: 30,
+  CountDays: 90,
 
   // 是否显示检测站点的链接
   ShowLink: true,
